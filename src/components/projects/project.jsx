721096a809c8f30efbefdbadf4cbff0bf813369a
Akhil <akhilm.jmu@yahoo.com>
2024-10-07 01:10:44 +0530

    added navbar, carousel

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -1,55 +1,60 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLink } from "@fortawesome/free-solid-svg-icons";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 
-import "./styles/project.css";
+import './styles/project.css';
 
 const Project = (props) => {
-	const { logo, title, description, linkText, link, liveLinkText, liveLink } =
-		props;
+   const { logo, title, description, linkText, link, liveLinkText, liveLink } = props;
 
-	return (
-		<React.Fragment>
-			<div className="project">
-				<>
-					<div className="project-container">
-						<div className="project-logo d-flex justify-content-between">
-							{logo.map((Logo, i) => (
-								<img
-									src={Logo}
-									alt="logo"
-									key={Logo}
-									className="mx-2"
-								/>
-							))}
-						</div>
-						<div className="project-title">{title}</div>
-						<div className="project-description">{description}</div>
-						<div className="project-link">
-							<div className="project-link-icon">
-								<FontAwesomeIcon icon={faLink} />
-							</div>
+   return (
+      <React.Fragment>
+         <div className='project'>
+            <>
+               <div className='project-container'>
+                  <div className='project-logo d-flex justify-content-between'>
+                     {logo.map((Logo, i) => (
+                        <img
+                           src={Logo}
+                           alt='logo'
+                           key={Logo}
+                           className='mx-2'
+                        />
+                     ))}
+                  </div>
+                  <div className='project-title'>{title}</div>
+                  <div className='project-description'>{description}</div>
+                  <div className='project-link'>
+                     <div className='project-link-icon'>
+                        <FontAwesomeIcon icon={faLink} />
+                     </div>
 
-							<a href={link} className="project-link-text">
-								{linkText}
-							</a>
-						</div>
+                     <a
+                        href={link}
+                        className='project-link-text'
+                     >
+                        {linkText}
+                     </a>
+                  </div>
 
-						<div className="project-link">
-							<div className="project-link-icon">
-								<FontAwesomeIcon icon={faLink} />
-							</div>
+                  <div className='project-link'>
+                     <div className='project-link-icon'>
+                        <FontAwesomeIcon icon={faLink} />
+                     </div>
 
-							<Link to={liveLink} className="project-link-text">
-								{liveLinkText}
-							</Link>
-						</div>
-					</div>
-				</>
-			</div>
-		</React.Fragment>
-	);
+                     <Link
+                        to={liveLink}
+                        className='project-link-text'
+                     >
+                        {liveLinkText}
+                     </Link>
+                  </div>
+               </div>
+            </>
+         </div>
+      </React.Fragment>
+   );
 };
 
 export default Project;
--- src/data/user.js
+++ src/data/user.js
@@ -1,80 +1,74 @@
 const INFO = {
-	main: {
-		title: "My Portfolio",
-		name: "Akhil Mahajan",
-		email: "akhilm.jmu@yahoo.com",
-		logo: "../logo.png",
-	},
+   main: {
+      title: 'My Portfolio',
+      name: 'Akhil Mahajan',
+      email: 'akhilm.jmu@yahoo.com',
+      logo: '../logo.png',
+   },
 
-	socials: {
-		twitter: "https://twitter.com/akhilm_jmu",
-		github: "https://github.com/akhil30-08",
-		linkedin: "https://www.linkedin.com/in/akhil-mahajan-b37a64249/",
-	},
+   socials: {
+      twitter: 'https://twitter.com/akhilm_jmu',
+      github: 'https://github.com/akhil30-08',
+      linkedin: 'https://www.linkedin.com/in/akhil-mahajan-b37a64249/',
+   },
 
-	homepage: {
-		title: "A full stack Web Developer and a Petroleum Engineer",
-		description:
-			"I am a Full Stack Web Developer and a Petroleum Engineer by qualification. I enjoy coding and Coffee. I love to build and collaborate to make good things. I would like to join you on board and work together to build cool stuffs.",
-	},
+   homepage: {
+      title: 'A full stack Web Developer and a Petroleum Engineer',
+      description:
+         'I am a Full Stack Web Developer and a Petroleum Engineer by qualification. I enjoy coding and Coffee. I love to build and collaborate to make good things. I would like to join you on board and work together to build cool stuffs.',
+   },
 
-	about: {
-		title: "I’m Akhil Mahajan. I live in what you all call Paradise on Earth - Jammu And Kashmir.   ",
-		description:
-			"After completing my education in Petroleum Engineering, I started exploring web development which is my favourite thing to do now. Since then I am constantly trying to learn new things from scratch and building side by side. And I cannot wait to join a team of people who are innnovative.",
-	},
+   about: {
+      title: 'I’m Akhil Mahajan. I live in what you all call Paradise on Earth - Jammu And Kashmir.   ',
+      description:
+         'After completing my education in Petroleum Engineering, I started exploring web development which is my favourite thing to do now. Since then I am constantly trying to learn new things from scratch and building side by side. And I cannot wait to join a team of people who are innnovative.',
+   },
 
-	articles: {
-		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
-		description:
-			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-	},
+   articles: {
+      title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+      description:
+         'Chronological collection of my long-form thoughts on programming, leadership, product design, and more.',
+   },
 
-	projects: [
-		{
-			title: "Snapgram",
-			description:
-				"A modern social app with a stunning UI with a native mobile feel, a special tech stack, an infinite scroll feature, and amazing performance using React JS, Appwrite, TypeScript, Ten-Stack Query and more.",
-			logo: [
-				"/1_Gh4eaAQU432ZQH7qsVbJ_A.webp",
-				"/62ed0da919dc399d5441e670.png",
-				"/tailwind-css-icon-2048x1229-u8dzt4uh.png",
-			],
-			linkText: "View Project",
-			link: "https://github.com/akhil30-08/SnapGram",
-			liveLinkText: "Live Link",
-			liveLink: "https://snapgram-pi-jet.vercel.app/sign-in",
-		},
-		{
-			title: "Nike-Tailwind",
-			description:
-				"A Nike like React FrontEnd App built with Vite to practice Tailwind. It uses Tailwind for layout and is built using JavaScript. It emphasize on basics of Tailwind CSS",
-			logo: [
-				"/tailwind-css-icon-2048x1229-u8dzt4uh.png",
-				"/react.png",
-				"/vite.jpeg",
-			],
-			linkText: "View Project",
-			link: "https://github.com/akhil30-08/React-Nike-App-with-Tailwind-CSS",
-			liveLinkText: "Live Link",
-			liveLink: "https://nike-reactapp-tailwind.onrender.com",
-		},
-		{
-			title: "Twitter-Clone",
-			description:
-				"A MERN stack twitter clone web app. It uses Express JS for backend, MongoDB to store database. React-Vite for frontend, bootstrap for designing and axios for calling APIs from backend.",
-			logo: [
-				"/react.png",
-				"/express.png",
-				"/mongodb.webp",
-				"/bootstrap.webp",
-			],
-			linkText: "View Project",
-			link: "https://github.com/akhil30-08/MERN-STACK-TWITTER-CLONE",
-			liveLinkText: "Live Link",
-			liveLink: "https://twitter-frontend-2nk4.onrender.com",
-		},
-	],
+   projects: [
+      {
+         title: 'Snapgram',
+         src: '/snapgram-img.jpg',
+         description:
+            'A modern social app with a stunning UI with a native mobile feel, a special tech stack, an infinite scroll feature, and amazing performance using React JS, Appwrite, TypeScript, Ten-Stack Query and more.',
+         logo: [
+            '/1_Gh4eaAQU432ZQH7qsVbJ_A.webp',
+            '/62ed0da919dc399d5441e670.png',
+            '/tailwind-css-icon-2048x1229-u8dzt4uh.png',
+         ],
+         linkText: 'View Project',
+         link: 'https://github.com/akhil30-08/SnapGram',
+         liveLinkText: 'Live Link',
+         liveLink: 'https://snapgram-pi-jet.vercel.app/sign-in',
+      },
+      {
+         title: 'Nike-Tailwind',
+         src: 'nike-tailwind-img.jpg',
+         description:
+            'A Nike like React FrontEnd App built with Vite to practice Tailwind. It uses Tailwind for layout and is built using JavaScript. It emphasize on basics of Tailwind CSS',
+         logo: ['/tailwind-css-icon-2048x1229-u8dzt4uh.png', '/react.png', '/vite.jpeg'],
+         linkText: 'View Project',
+         link: 'https://github.com/akhil30-08/React-Nike-App-with-Tailwind-CSS',
+         liveLinkText: 'Live Link',
+         liveLink: 'https://nike-reactapp-tailwind.onrender.com',
+      },
+      {
+         title: 'Twitter-Clone',
+         src: '/twitter-img.jpg',
+         description:
+            'A MERN stack twitter clone web app. It uses Express JS for backend, MongoDB to store database. React-Vite for frontend, bootstrap for designing and axios for calling APIs from backend.',
+         logo: ['/react.png', '/express.png', '/mongodb.webp', '/bootstrap.webp'],
+         linkText: 'View Project',
+         link: 'https://github.com/akhil30-08/MERN-STACK-TWITTER-CLONE',
+         liveLinkText: 'Live Link',
+         liveLink: 'https://twitter-frontend-2nk4.onrender.com',
+      },
+   ],
 };
 
 export default INFO;
